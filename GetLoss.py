1d857e5c9bb0f2ca151a8620bb941e68812edc9a
dirflash <aaron.e.davis@gmail.com>
2019-01-21 19:16:06 -0600

    MX68 Wan interface loss percentage

--- GetLoss.py
+++ GetLoss.py
@@ -1,5 +1,5 @@
 # This script pulls the loss percentage off of the WAN interface on
-# my Meraki MX68 firewall.  Your milage may vary.
+# my Meraki MX68 firewall.  Your mileage may vary.
 
 import json
 import requests
