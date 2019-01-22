c559566571fe07d035992d4456f7ffa79082c97a
Aaron Davis <dirflash@users.noreply.github.com>
2019-01-21 22:02:26 -0600

    Add files via upload

--- GetLoss.py
+++ GetLoss.py
@@ -3,12 +3,21 @@
 
 import json
 import requests
+import tweepy 
 
-
+# Meraki
 keys_file = open("keys.txt")
 lines = keys_file.readlines()
 Meraki_API_Key = lines[0].rstrip()
 LandLurl = lines[1].rstrip()
+
+# Tweepy
+consumer_key = lines[2].rstrip()
+consumer_secret = lines[3].rstrip()
+access_token = lines[4].rstrip()
+access_token_secret = lines[5].rstrip()
+status_msg = "Hello Everyone! Using a bit of Python programming.  I am able to determine that the loss percentage for the WAN interace on my Meraki Lab3 MX68 is "
+
 payload = ""
 
 querystring = {
@@ -39,3 +48,13 @@ lp = response_data[last_list]['lossPercent']
 loss_percent = json.dumps(lp).replace('"', '')
 
 print("Loss percentage for 'Lab3 MX68' is {}".format(loss_percent))
+
+# Tweepy authentication of consumer key and secret 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+  
+# Tweepy authentication of access token and secret 
+auth.set_access_token(access_token, access_token_secret) 
+api = tweepy.API(auth) 
+  
+# update the status 
+api.update_status(status = status_msg + "{}%.".format(loss_percent)) 
